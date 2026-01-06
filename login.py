9f46df8cdae32f45205a3a9e51e5c5489d61d67b
jasmineu-2006 <jasmine.u2502@gmail.com>
2026-01-06 22:56:59 +0530

    Added basic login feature

---
+++ login.py
@@ -0,0 +1,10 @@
+def login():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if username == "admin" and password == "admin123":
+        print("Login successful")
+        return True
+    else:
+        print("Invalid credentials")
+        return False
