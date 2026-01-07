52e19f69d2f9e18492cfa4551bb09773446b3e25
jasmineu-2006 <jasmine.u2502@gmail.com>
2026-01-07 23:17:55 +0530

    Teammate updated login module

--- login.py
+++ login.py
@@ -1,12 +1,12 @@
 def login():
-
+    print("Login Module by teamate")
     username = input("Enter username: ")
     password = input("Enter password: ")
 
     if username == "admin" and password == "admin123":
         print("Login successful")
         return True
-    else if:
+    
     else:
         print("Invalid credentials")
         return False
